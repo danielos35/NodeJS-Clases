812b476d5848ff1bc0709a78899cd6ba541273d5
Daniel <studiosdopplermusic@gmail.com>
2022-05-23 19:29:33 -0500

    ventajas

--- Notas/intro.js
+++ Notas/intro.js
@@ -1,1 +1,22 @@
 // Intro node JS
+
+/*
+¿Que es el backEnd?....................................................
+
+- Es una aplicación que se ejecuta en el servidor.
+- API, consumir recuros de un servidor. 
+
+*/
+
+/*
+Ventajas.
+
+- Crear aplicaciones escalables. 
+- Excelente prototipado.
+- Desarrollo agil. 
+- Ejecución rapida. 
+- Aplicaciones con menos personas 
+- 35% menos lineas de codigo, 40% menos archivos.
+- Tiempo de respuesta hasta un 35 % mas rapido. 
+
+*/
