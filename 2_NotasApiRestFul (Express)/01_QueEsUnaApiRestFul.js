ceb520395f22e001fa356f215d2d6239bec48d9d
Daniel <studiosdopplermusic@gmail.com>
2022-05-30 14:43:46 -0500

    notas api

---
+++ 2_NotasApiRestFul (Express)/01_QueEsUnaApiRestFul.js
@@ -0,0 +1,25 @@
+/*
+
+API RESTFUL 
+
+COMPONENTES DE UNA APLICACIÓN 
+- Cliente.
+- Servidor.
+
+COMUNICACIÓN ENTRE COMPONENTES CLIENTE-SERVIDOR 
+- Se comunican por medio del protocolo HTTP o HTTPs para conexión segura/
+
+SERVIDOR
+- Presta un servicio que está a la espera de peticiones de parte del cliente.
+- Estas llamadas al servidor se hacen por medio de AJAX.
+
+OPERACIONES BASICAS PARA INTERCAMBIO DE DATOS (Operaciones CRUD), Y PETICIONES POSIBLES
+- Create -> Post. 
+- Read -> Get.
+- Update -> Put. 
+- Delete -> Delete.
+
+BUENAS PRACTICAS.
+- Como buenas practicas se recomienda colocar api en la url de la aplicación ejemplo: maraldigital.com/api   
+
+*/
---
+++ 2_NotasApiRestFul (Express)/02_express.js
@@ -0,0 +1,19 @@
+/*
+EXPRESS
+- Es un framework que funciona gracias a NodeJS (este trabaja en tiempo de ejecución y es multiplataforma, permitiendo crear herramientas del lado delservidor como un API), para la creación de API REST se puede usar el modulo HTTP de node pero el framework facilita su uso. 
+- Espress también es el framework mas popular para API dandole una gran cantidad de herramientas para el manejo de los datos.
+- Se puede incluir también middelwares. 
+
+DOCUMENTACIÓN 
+https://expressjs.com/
+
+INSTALACIÓN
+
+npm i express
+
+NOTA.
+- npm init --yes (Crea el proyecto saltando todos los pasos de configuración). 
+
+
+
+*/
