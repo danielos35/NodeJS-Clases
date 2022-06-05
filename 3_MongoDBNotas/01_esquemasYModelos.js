0e87e8a4c49e8e427cf2f118b9810a9ee62ef951
Daniel <studiosdopplermusic@gmail.com>
2022-06-05 16:27:18 -0500

    esquemas y modelos

---
+++ 3_MongoDBNotas/01_esquemasYModelos.js
@@ -0,0 +1,10 @@
+/*
+COLECIÓN 
+- En las bases de datos una colección, es lo equivalente a una tabla.
+- Los documentos dentro de las BD no relacionales equivale a las filas.
+
+
+ESQUEMA.
+- Permite definir la estructura que va tener un documento dentro de nuestra colección.
+
+*/
--- mongoDB/app.js
+++ mongoDB/app.js
@@ -1,10 +1,28 @@
-// Usar mongoose
+// Usar mongoose.
 const mongoose = require("mongoose");
 
-// Conectarnos a mongo
+// Conectarnos a mongo.
 mongoose
   .connect("mongodb://localhost/demo")
   .then(() => {
     console.log("conectado a mongo");
   })
   .catch((err) => console.log(`No se pudo conectar por un error ${err}`));
+
+// Creación del esquema con mogoose.
+const cursoSchema = new mongoose.Schema({
+  nombre: String,
+  autor: String,
+  etiquetas: [String],
+  fecha: { type: Date, default: Date.now },
+  publicado: Boolean,
+});
+
+// Creación de la clase.
+const Curso = mongoose.model("curso", cursoSchema);
+const curso = new Curso({
+  nombre: "JavaScript",
+  autor: "Daniel",
+  etiquetas: ["Desarrollo web", "forntEnd"],
+  publicado: true,
+});
