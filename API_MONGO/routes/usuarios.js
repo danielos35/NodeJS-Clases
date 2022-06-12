c7e893bf308fe068711c3f634c6fc822d0550163
Daniel <studiosdopplermusic@gmail.com>
2022-06-11 19:13:38 -0500

    post - put - delete - get Usuarios

--- API_MONGO/routes/usuarios.js
+++ API_MONGO/routes/usuarios.js
@@ -2,7 +2,16 @@ const express = require("express");
 const Usuario = require("../models/usuario_model");
 const ruta = express.Router();
 ruta.get("/", (req, res) => {
-  res.json("Listo el GET de usuarios.");
+  let resultado = verUsuarios();
+  resultado
+    .then((usuarios) => {
+      res.json(usuarios);
+    })
+    .catch((err) => {
+      res.status(400).json({
+        err: err,
+      });
+    });
 });
 
 ruta.post("/", (req, res) => {
@@ -22,7 +31,7 @@ ruta.post("/", (req, res) => {
     });
 });
 
-// POST
+// POST (Crear nuevos usuarios)
 let crearUsuario = async function (body) {
   let usuario = new Usuario({
     email: body.email,
@@ -33,4 +42,70 @@ let crearUsuario = async function (body) {
   return await usuario.save();
 };
 
+// GET
+let verUsuarios = async function () {
+  let usuarios = await Usuario.find({ estado: true });
+  return usuarios;
+};
+
+// PUT (actualizar)
+ruta.put("/:email", (req, res) => {
+  let resultado = actualizarUsuarios(req.params.email, req.body);
+  resultado
+    .then((valor) => {
+      res.json({
+        valor: valor,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        error: err,
+      });
+    });
+});
+
+let actualizarUsuarios = async function (email, body) {
+  let usuario = await Usuario.findOneAndUpdate(
+    email,
+    {
+      $set: {
+        nombre: body.nombre,
+        password: body.password,
+      },
+    },
+    { new: true }
+  );
+
+  return usuario;
+};
+
+// DELETE
+ruta.delete("/:email", (req, res) => {
+  let resultado = desactivarUsuario(req.params.email);
+  resultado
+    .then((valor) => {
+      res.json({
+        usuario: valor,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        error: err,
+      });
+    });
+});
+
+let desactivarUsuario = async function (email) {
+  let usuario = await Usuario.findOneAndUpdate(
+    email,
+    {
+      $set: {
+        estado: false,
+      },
+    },
+    { new: true }
+  );
+
+  return usuario;
+};
 module.exports = ruta;
