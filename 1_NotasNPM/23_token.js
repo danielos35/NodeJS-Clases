5e6081cd58b36dd72223120216a29f54f5586d46
Daniel <studiosdopplermusic@gmail.com>
2022-06-13 21:39:22 -0500

    notas dos

---
+++ 1_NotasNPM/23_token.js
@@ -0,0 +1,33 @@
+/*
+
+
+TOKEN: 
+- Una tecnologia que nos permite hacer una validación expecifica para un usuario determinado. 
+
+
+JWT (JSON WEB TOKENS)
+- Es una tecnologia que nos permite generar tokens de lado del servidor utilizando tecnologia JS 
+
+-header
+-- Algoritmo 
+-- Tipo de token
+
+-Payload
+-- Data
+-- Usuario
+-- roles.
+
+
+-Signatur
+-- header y payload codificados en base 64
+  
+Cibergrafia
+- https://jwt.io/
+
+-Decodificar datos
+--https://www.base64decode.org/
+
+
+INSTALAR JWT
+npm i jsonwebtoken
+*/
